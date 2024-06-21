8589d6e003892ff2208fb5a7a78437d96bec96cf
Hasan <hsmusayev@gmail.com>
2024-06-21 17:54:34 +0400

    added other details inside placesPage

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const User = require('./models/User.js');
 const cookieParser = require('cookie-parser');
+const imageDownloader = require('image-downloader');
 require('dotenv').config();
 const app = express();
 
@@ -13,6 +14,7 @@ const jwtSecret = 'hasanhasan123hasan';
 
 app.use(express.json());
 app.use(cookieParser());
+app.use('/uploads', express.static(__dirname + '/uploads'));
 app.use(cors({
     credentials: true,
     origin: 'http://localhost:5173'
@@ -24,6 +26,15 @@ app.get('/test', (req, res) => {
     res.json('test ok');
 });
 
+function genToken(length) {
+    const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let token = '';
+    for (let i = 0; i < length; i++) {
+      token += chars.charAt(Math.floor(Math.random() * chars.length));
+    }
+    return token;
+  }
+  
 app.post('/register', async (req,res)=> {
     const {name,email,password} = req.body;
     try {
@@ -84,3 +95,14 @@ app.get('/profile', (req,res) => {
 app.post('/logout', (req,res) => {
     res.cookie('token', '').json(true);
 });
+
+app.post('/upload-by-link', async (req, res) => {
+    const {link} = req.body;
+    const newName = 'photo_' + Date.now() +'.jpg';
+    await imageDownloader.image({
+        url: link,
+        dest: __dirname + '/uploads/' + newName,
+    });
+    res.json(newName);
+
+})
